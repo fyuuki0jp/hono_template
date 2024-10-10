614d34b5f428f2cdff63c87fc5c8a61b50e591bf
fyuuki0jp <fuyu0307.jp@gmail.com>
2024-10-10 22:26:15 +0000

    [add] tailwnd css and biome

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,38 @@
 import devServer from '@hono/vite-dev-server'
+import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss'
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ mode }) => {
-    if (mode === 'client') {
-        return {
-            build: {
-                rollupOptions: {
-                    input: '/src/app/index.tsx',
-                    output: {
-                        entryFileNames: 'static/client.js'
-                    }
-                }
-            }
-        }
-    } else {
-        return {
-            server: {
-                host: true
-            },
-            resolve: {
-                alias: {
-                  react: 'hono/jsx/dom',
-                  'react-dom': 'hono/jsx/dom'
-                }
-            },
-            plugins:[
-                devServer({
-                    entry: "/src/main.ts"
-                })
-            ]
-        }
+  let config = {}
+  if (mode === 'client') {
+    config = {
+      build: {
+        rollupOptions: {
+          input: '/src/app/index.tsx',
+          output: {
+            entryFileNames: 'static/client.js',
+          },
+        },
+      },
     }
+  } else {
+    config = {
+      server: {
+        host: true,
+      },
+      resolve: {
+        alias: {
+          react: 'hono/jsx/dom',
+          'react-dom': 'hono/jsx/dom',
+        },
+      },
+      plugins: [
+        devServer({
+          entry: '/src/main.ts',
+        }),
+      ],
+    }
+  }
+  return config
 })
